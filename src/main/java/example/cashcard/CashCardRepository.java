c4d5fd48addd7a613897ab6eb6ddaafc0c013255
mithwick93 <mithwick93@gmail.com>
2024-01-16 15:52:03 +0100

    Add repository level filter by owner for get all endpoint
    Signed-off-by: mithwick93 <mithwick93@gmail.com>

--- src/main/java/example/cashcard/CashCardRepository.java
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -1,9 +1,7 @@
 package example.cashcard;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -17,13 +15,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface CashCardRepository extends CrudRepository<CashCard, Long> {
-
-    Iterable<CashCard> findByOwner(String owner);
-
-    default Iterable<CashCard> findAll() {
-        SecurityContext context = SecurityContextHolder.getContext();
-        Authentication authentication = context.getAuthentication();
-        String owner = authentication.getName();
-        return findByOwner(owner);
-    }
+    @Query("select * from cash_card cc where cc.owner = :#{authentication.name}")
+    Iterable<CashCard> findAll();
 }
